7d0b3e56f9c2a46998d215dad9567a2e34c888b3
Nitin Pabba <nitin.pabba@highjump.com>
2019-09-03 01:11:00 -0400

    Initial commit

---
+++ GeniusPlazaCoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeniusPlazaCoding
+//
+//  Created by Nitin Pabba on 9/3/19.
+//  Copyright © 2019 Nitin Pabba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
